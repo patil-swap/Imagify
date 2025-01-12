1b4bc8d9a1b798a0bee85c4de022b6992ca884dd
Swapnil Patil <patil.k.swapnil@gmail.com>
2025-01-11 20:48:30 -0330

    Adds framer motion to project and animation to header section of homepage

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,19 +1,57 @@
+import { useContext } from 'react'
 import { assets } from '../assets/assets'
+import { motion } from "motion/react"
+import { AppContext } from "../context/AppContext"
+import { useNavigate } from 'react-router-dom'
 
 const Header = () => {
+
+  const { user, setShowLogin } = useContext(AppContext)
+  const navigate = useNavigate()
+
+  const onClickHandler = () => {
+	if (user) {
+		navigate('/result')
+	} else {
+		setShowLogin(true)
+	}
+  }
   return (
-	<div className='flex flex-col justify-center items-center text-center my-20'>
-		<div className='text-stone-500 inline-flex text-center gap-2 bg-white px-6 py-1 rounded-full border border-neutral-500'>
+	<motion.div className='flex flex-col justify-center items-center text-center my-20'
+	initial={{opacity: 0.2, y: 100}}
+	transition={{duration: 1}}
+	whileInView={{opacity: 1, y: 0}}
+	viewport={{once: true}}
+	>
+		<motion.div className='text-stone-500 inline-flex text-center gap-2 bg-white px-6 py-1 rounded-full border border-neutral-500'
+		initial={{opacity: 0, y: -20}}
+		animate={{opacity: 1, y: 0}}
+		transition={{delay: 0.2, duration: 0.8}}
+		>
 			<p>Best Text to Image Generator</p>
 			<img src={assets.star_icon} className='w-5' alt="" />
-		</div>
-		<h1 className='text-4xl max-w-[300px] sm:text-7xl sm:max-w-[590px] mx-auto mt-10 text-center'>Turn text to <span className='text-blue-600'>Image</span>, in seconds.</h1>
-		<p className='text-center max-w-xl mx-auto mt-5'>Unleash your creativity with AI. Turn your imagination into visual art in seconds - just type, and watch the magic happen.</p>
+		</motion.div>
+		<motion.h1 className='text-4xl max-w-[300px] sm:text-7xl sm:max-w-[590px] mx-auto mt-10 text-center'
+		initial={{opacity: 0}}
+		animate={{opacity: 1}}
+		transition={{delay: 0.4, duration: 2}}
+		>Turn text to <span className='text-blue-600'>Image</span>, in seconds.</motion.h1>
+		<motion.p className='text-center max-w-xl mx-auto mt-5'
+		initial={{opacity: 0}}
+		animate={{opacity: 1}}
+		transition={{delay: 0.6, duration: 2}}
+		>Unleash your creativity with AI. Turn your imagination into visual art in seconds - just type, and watch the magic happen.</motion.p>
 		
-		<button className='sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full'>
+		<motion.button onClick={onClickHandler} className='sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full'
+		whileHover={{scale: 1.05}}
+		whileTap={{scale: 0.95}}
+		initial={{opacity: 0}}
+		animate={{opacity: 1}}
+		transition={{default: {duration: 0.5}, opacity: {delay: 0.8, duration: 1}}}
+		>
 			Generate Images 
 			<img src={assets.star_group} className='h-6' alt="" />
-		</button>
+		</motion.button>
 
 		<div className='flex flex-wrap justify-center mt-16 gap-3'>
 			{Array(6).fill('').map((item, index) => (
@@ -22,7 +60,7 @@ const Header = () => {
 			))}
 		</div>
 		<p className='mt-2 text-neutral-600'>Generated Images from Imagify</p>
-	</div>
+	</motion.div>
   )
 }
 
