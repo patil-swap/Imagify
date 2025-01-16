43bf09281261a1c9d44326152b4c7353fa918a09
Swapnil Patil <patil.k.swapnil@gmail.com>
2025-01-16 10:48:43 -0330

    Connects image generation AI api to frontend

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -4,6 +4,7 @@
 import axios from "axios";
 import { createContext, useEffect, useState } from "react";
 import { toast } from "react-toastify";
+import { useNavigate } from 'react-router-dom';
 
 export const AppContext = createContext();
 
@@ -12,6 +13,8 @@ const AppContextProvider = (props) => {
 	const [showLogin, setShowLogin] = useState(false)
 	const [token, setToken] = useState(localStorage.getItem('token'))
 	const [credit, setCredit] = useState(false)
+
+	const navigate = useNavigate()
 
 	const backendUrl = import.meta.env.VITE_BACKEND_URL
 	const loadCreditData = async () => {
@@ -23,6 +26,25 @@ const AppContextProvider = (props) => {
 			}
 		} catch(error) {
 			console.log(error)
+			toast.error(error.message)
+		}
+	}
+
+	const generateImage = async(prompt) => {
+		try{
+			const { data } = await axios.post(backendUrl + '/api/image/generate-image', {prompt}, {headers: {token}})
+			if(data.success) {
+				loadCreditData()
+				return data.resultImage
+			}
+			else {
+				toast.error(data.message)
+				loadCreditData()
+				if(data.creditBalance === 0) {
+					navigate('/buy')
+				}
+			}
+		} catch(error) {
 			toast.error(error.message)
 		}
 	}
@@ -40,7 +62,7 @@ const AppContextProvider = (props) => {
 	}, [token])
 
 	const value = {
-		user, setUser, showLogin, setShowLogin, backendUrl, token, setToken, credit, setCredit, logout
+		user, setUser, showLogin, setShowLogin, backendUrl, token, setToken, credit, setCredit, logout, generateImage
 	}
 
 	return (
--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import { assets } from '../assets/assets'
 import { motion } from "motion/react"
+import { AppContext } from '../context/AppContext'
 
 const Result = () => {
 
@@ -8,9 +9,20 @@ const Result = () => {
   const [isImageLoaded, setIsImageLoaded] = useState(false)
   const [loading, setLoading] = useState(false)
   const [input, setInput] = useState('')
+
+  const { generateImage } = useContext(AppContext)
   
   const onSubmitHandler = async (e) => {
-
+	e.preventDefault()
+	setLoading(true)
+	if(input) {
+		const image = await generateImage(input)
+		if(image) {
+			setIsImageLoaded(true)
+			setImage(image)
+		}
+	}
+	setLoading(false)
   }
 
   return (
